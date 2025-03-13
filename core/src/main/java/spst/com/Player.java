5bdc48c92b5fb06cd26f9bf53ad82f4242a28b2f
Quan dz <tranbaoquan.102012@gmail.com>
2025-03-13 23:47:08 +0700

    update trong cay

--- core/src/main/java/spst/com/Player.java
+++ core/src/main/java/spst/com/Player.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.math.Polygon;
+import spst.com.Roads.Tree;
 
 import java.awt.*;
 
@@ -50,8 +51,11 @@ public class Player extends MyActor {
         if (Gdx.input.isTouched()) {
             mouseX = mouse.x;
             mouseY = mouse.y;
+            if(Master.preparePlant){
+                new Tree(mouseX,mouseY,getStage());
+            }
         }
-        if (mouseX != -13314 && mouseY != -1321687 ){
+        if (mouseX != -13314 && mouseY != -1321687 && !Master.preparePlant){
             if (getX()-2 < mouseX && mouseX< getX()+2) {
                 if (!(getY()-2 < mouseY && mouseY< getY()+2)) {
                     if (getY()-2 < mouseY) {

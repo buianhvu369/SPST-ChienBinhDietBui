c78dc397df83a811756b2c5caa5e7e10eb261ef6
Quan dz <tranbaoquan.102012@gmail.com>
2025-03-15 16:42:12 +0700

    luu tam

---
+++ core/src/main/java/spst/com/LoadingPlant.java
@@ -0,0 +1,10 @@
+package spst.com;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class LoadingPlant extends MyActor{
+    int timeLoad = 120;
+    public LoadingPlant(float x, float y, Stage s) {
+        super(x, y, s);
+    }
+}

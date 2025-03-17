9ca89576fb2218bf7a49f138b9f18b6753d9c4bc
nhatdac <nhatdac22@gmail.com>
2025-03-17 16:21:11 +0700

    man cut tree

---
+++ core/src/main/java/spst/com/People/People.java
@@ -0,0 +1,38 @@
+package spst.com.People;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import spst.com.MyActor;
+import spst.com.Screen.Master;
+import spst.com.Utils;
+
+public class People extends MyActor {
+    boolean isCutting = false;
+    boolean isFiring = false;
+    int time = 0;
+    public People(float x, float y, Stage s) {
+        super(x, y, s);
+        textureRegion = Utils.getRegion( 23*16 + 16* MathUtils.random(0, 3),MathUtils.random(0, 17)*16,16,16);
+        setSize(32, 32);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        time ++;
+
+        if(time % 300 == 0){
+            time = 0;
+            isCutting = MathUtils.randomBoolean();
+            if(isCutting){
+                if(!Master.trees.isEmpty()){
+                    System.out.println("Chat cay thoi anh em!");
+                    setPosition(Master.trees.get(MathUtils.random(0, Master.trees.size - 1)).getX() + 32, Master.trees.get(MathUtils.random(0, Master.trees.size - 1)).getY());
+                }
+            } else {
+                setPosition(MathUtils.random(0, 2000), MathUtils.random(0, 700));
+            }
+        }
+    }
+}

32841812084b34c64fad3221e9bc9e0b9cd56fbd
Quan dz <tranbaoquan.102012@gmail.com>
2025-03-17 21:47:52 +0700

    may loc kk

--- core/src/main/java/spst/com/MayLoc.java
+++ core/src/main/java/spst/com/MayLoc.java
@@ -1,12 +1,14 @@
 package spst.com;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class MayLoc extends MyActor{
+    Texture mayloc = new Texture("maylockhongkhi.png");
     public MayLoc(float x, float y, Stage s,float width, float height) {
         super(x, y, s);
-        Texture mayloc = new Texture("maylockhongkhi.png");
+        textureRegion = new TextureRegion(mayloc);
         setSize(width,height);
     }
 }

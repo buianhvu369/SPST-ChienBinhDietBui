57937b2fc2c1a3835fcb56d540b08ef34d42d311
nhatdac <nhatdac22@gmail.com>
2025-03-18 18:16:39 +0700

    update

---
+++ core/src/main/java/spst/com/Direction.java
@@ -0,0 +1,8 @@
+package spst.com;
+
+public enum Direction {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
--- core/src/main/java/spst/com/People/People1.java
+++ core/src/main/java/spst/com/People/People1.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import spst.com.AnimationClickMouse;
+import spst.com.Direction;
 import spst.com.MyActor;
 import spst.com.Roads.Tree;
 import spst.com.Utils;
@@ -16,11 +18,12 @@ public class People1 extends MyActor {
     Animation<TextureRegion> animationRight;
     Animation<TextureRegion> animationUp;
     Animation<TextureRegion> animationDown;
-    int speed = 2;
     float time;
-    float mouseX = -13314;
-    float mouseY = -1321687;
-    boolean takeWater = true;
+    int timeDirection;
+
+    Direction direction = Direction.LEFT;
+    float speedX = 0;
+    float speedY = 0;
 
     public People1(float x, float y, Stage s, boolean rightside) {
         super(x, y, s);
@@ -41,72 +44,46 @@ public class People1 extends MyActor {
         textureRegion = animationRight.getKeyFrame(time);
     }
 
-   // @Override
-   // public void act(float delta) {
-       // super.act(delta);
-
-        //if (takeWater) {
-           // mouseX =Master.giengleft.getX() ;
-            //mouseY = Master.giengleft.getY() ;
-       // }
-       // if (mouseX != -13314 && mouseY != -1321687){
-        ///    if (Math.abs(getX()-mouseX) < Math.abs(getY()-mouseY)) {
-     ///           if (!(getX() - 2 < mouseX && mouseX < getX() + 2)) {
-   //                 if (mouseX < getX() + 2) {
- //                       if (!(32 * 16 <= getY() && getY() < 32 * 23 && getX() <= 32 * 9)) {
-                         ///   moveBy(-speed, 0);
-                      ///  }
-                   ///     time += delta;
-                ///        textureRegion = animationLeft.getKeyFrame(time);
-             ///       } else if (mouseX > getX() - 2) {
-          ///              moveBy(speed, 0);
-        //                time += delta;
-     ///                   textureRegion = animationRight.getKeyFrame(time);
-   //                 }
- //               } else if (!(getY() - 2 < mouseY && mouseY < getY() + 2)) {
-                  //  if (getY() - 2 < mouseY) {
-                     //   if (!(0 <= getX() && getX() < 32 * 9 && getY() > 32 * 16 - 8 && getY() <= 800 - 32 * 4)) {
-                          //  moveBy(0, speed);
-                       /// }
-                        ///time += delta;
-                       // textureRegion = animationUp.getKeyFrame(time);
-                  //  } else if (mouseY < getY() + 2) {
-                        //if (!(0 <= getX() && getX() < 32 * 9 && getY() <= 800 - 32 * 2 && getY() >= 32 * 18)) {
-                         //   moveBy(0, -speed);
-                       // }
-                      //  time += delta;
-                       // textureRegion = animationDown.getKeyFrame(time);
-                  //  }
-              //  }
-           // } else {
-               // if (!(getY() - 2 < mouseY && mouseY < getY() + 2)) {
-                    //if (getY() - 2 < mouseY) {
-                       //if (!(0 <= getX() && getX() < 32 * 9 && getY() > 32 * 16 - 8 && getY() <= 800 - 32 * 4)) {
-                      //     moveBy(0, speed);
-                      // }
-                       // time += delta;
-                        ///textureRegion = animationUp.getKeyFrame(time);
-                    ///} else if (mouseY < getY() + 2) {
-                       /// if (!(0 <= getX() && getX() < 32 * 9 && getY() <= 800 - 32 * 2 && getY() >= 32 * 18)) {
-                        ///    moveBy(0, -speed);
-                        ///}
-                        //time += delta;
-                        ///textureRegion = animationDown.getKeyFrame(time);
-                   // }
-                //} else if (!(getX() - 2 < mouseX && mouseX < getX() + 2)) {
-                  ///  if (mouseX < getX() + 2) {
-                       // if (!(32 * 16 <= getY() && getY() < 32 * 23 && getX() <= 32 * 9)) {
-                        //    moveBy(-speed, 0);
-                        //}
-                        //time += delta;
-                        //textureRegion = animationLeft.getKeyFrame(time);
-                   // } else if (mouseX > getX() - 2) {
-                        //moveBy(speed, 0);
-                      ///  time += delta;
-                      //  textureRegion = animationRight.getKeyFrame(time);
-                    //}
-                //}
-            //}
-        //}
-   // }
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        time+= delta;
+        switch (direction){
+            case UP -> {
+                speedX = 0;
+                speedY = 1;
+                textureRegion = animationUp.getKeyFrame(time);
+            }
+            case DOWN -> {
+                speedX = 0;
+                speedY = -1;
+                textureRegion = animationDown.getKeyFrame(time);
+            }
+            case LEFT -> {
+                speedX = -1;
+                speedY = 0;
+                textureRegion = animationLeft.getKeyFrame(time);
+            }
+            case RIGHT -> {
+                speedX = 1;
+                speedY = 0;
+                textureRegion = animationRight.getKeyFrame(time);
+            }
+        }
+        moveBy(speedX, speedY);
+        if(getX()< 0){
+            direction = Direction.RIGHT;
+        } else if (getX()> 2400) {
+            direction = Direction.LEFT;
+        } else if (getY()> 800) {
+            direction = Direction.DOWN;
+        } else if (getY() < 0) {
+            direction = Direction.UP;
+        }
+        timeDirection++;
+        if(timeDirection % 300 == 0){
+            timeDirection = 0;
+            direction = Direction.values()[MathUtils.random(0, Direction.values().length -1)];
+        }
+    }
 }

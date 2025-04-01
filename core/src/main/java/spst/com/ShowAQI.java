712f6957b38b0659e114ac8fec955f3da3ce8eab
Quan dz <tranbaoquan.102012@gmail.com>
2025-04-01 17:21:53 +0700

    chinh thanh AQI:

--- core/src/main/java/spst/com/ShowAQI.java
+++ core/src/main/java/spst/com/ShowAQI.java
@@ -11,14 +11,14 @@ public class ShowAQI extends MyActor {
         super(x,y,s);
         setColor(Color.RED);
         textureRegion = Utils.getRegion(16*9,16,16,16);
-        setSize(textureRegion.getRegionWidth(), textureRegion.getRegionHeight());
-        setScale(100,0.5f);
+        setSize(Gdx.graphics.getWidth(), textureRegion.getRegionHeight());
+        setScale(1f,0.5f);
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
-        setScale(Master.AQI/10f,0.5f);
+        setScale(Master.AQI/300f,0.5f);
         if(Master.AQI<=50){
             setColor(Color.GREEN);
         }else if(Master.AQI<=100){
@@ -28,9 +28,7 @@ public class ShowAQI extends MyActor {
         }else if(Master.AQI<=200){
             setColor(Color.RED);
         }else if(Master.AQI<=300){
-                setColor(Color.PURPLE);
-        }else if(Master.AQI<=500){
-            setColor(Color.BROWN);
+                setColor(Color.BROWN);
         }
     }
 }
